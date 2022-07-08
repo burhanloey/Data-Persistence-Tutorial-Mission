13051efa93ce390d1ef4b02311ab31acd539646e
Burhanuddin Baharuddin <burhanloey@gmail.com>
2022-07-09 05:26:16 +0800

    Change best score from colon to dash and focus UI during startup

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -3,23 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 
 public class MenuUIHandler : MonoBehaviour
 {
     public TMP_Text BestScoreText;
     public TMP_InputField NameInputField;
+    public Button StartButton;
 
     // Start is called before the first frame update
     void Start()
     {
         NameInputField.text = GameManager.Instance.CurrentName;
 
+        if (string.IsNullOrEmpty(NameInputField.text))
+        {
+            NameInputField.Select();
+        }
+        else
+        {
+            StartButton.Select();
+        }
+
         if (GameManager.Instance.Data.HighestScore > 0)
         {
             string hiScoreName = GameManager.Instance.Data.HiScoreName;
             int highestScore = GameManager.Instance.Data.HighestScore;
 
-            BestScoreText.text = $"Best Score: {hiScoreName} : {highestScore}";
+            BestScoreText.text = $"Best Score: {hiScoreName} - {highestScore}";
             BestScoreText.gameObject.SetActive(true);
         }
     }
