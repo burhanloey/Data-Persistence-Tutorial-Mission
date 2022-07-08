b89a634a00874ce758b55817e79f6017e610675b
Burhanuddin Baharuddin <burhanloey@gmail.com>
2022-07-09 04:29:32 +0800

    Format file, use CRLF line ending

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -17,10 +17,10 @@ public class MainManager : MonoBehaviour
     private int m_HighestScore;
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,8 +35,8 @@ public class MainManager : MonoBehaviour
 
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
